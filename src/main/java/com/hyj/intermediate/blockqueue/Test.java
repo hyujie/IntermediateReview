fd6e61aae2664bf7aac9a796b980e5265d8ee4e1
huangyujie <190158792@qq.com>
2019-03-05 18:12:06 +0800

    first commit

---
+++ src/main/java/com/hyj/intermediate/blockqueue/Test.java
@@ -0,0 +1,18 @@
+package com.hyj.intermediate.blockqueue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @version 1.0
+ * @Authord yujie huang  email: 190158792@qq.com
+ * @Description
+ * @Date Create by in 17:02 2019/3/5
+ */
+public class Test {
+
+    public void test(){
+        ReentrantLock lock  = new ReentrantLock();
+        lock.lock();
+    }
+}
